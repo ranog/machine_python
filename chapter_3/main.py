305db93e8be2345f49bc74f2190b1596e0de34d0
ranog <jprnogueira@yahoo.com.br>
2017-12-05 20:06:43 -0200

    Training a perceptron via scikit-learn

--- chapter_3/main.py
+++ chapter_3/main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from plot_decision_regions import plot_decision_regions
 from sklearn import datasets
-from sklearn.cross_validation import train_test_split
+# cross_validation: Foi substituido pelo model_selection
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
@@ -20,10 +22,21 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+# n_iter: Foi substituido por max_iter
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
 ppn.fit(X_train_std, y_train)
 
 y_pred = ppn.predict(X_test_std)
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
 
 print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn,
+        test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
